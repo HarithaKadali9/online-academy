5841f4899d4e8bd240464b4fa8c779bc28f95929
HarithaKadali9 <kadaliharitha23@gmail.com>
2025-04-24 11:13:09 +0530

    added a small change in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className='w-full fixed bottom-0 left-0 z-50'>
+    <footer className='w-full'>
       <div className='max-w-[1200px] mx-auto flex flex-col items-center justify-center py-6 space-y-4'>
         <div className='flex space-x-6'>
           <a href="https://github.com/HarithaKadali9" target="_blank" rel="noopener noreferrer">
@@ -20,7 +20,7 @@ function Footer() {
         </div>
         <p className='text-sm'>&copy; {currentYear} All rights reserved</p>
       </div>
-    </div>
+    </footer>
   );
 }
 
